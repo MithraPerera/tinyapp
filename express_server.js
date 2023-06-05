747bfd3b1998468b7f6239bef7e1bd915a44432a
Mithra Perera <mithraperera@hotmail.com>
2023-06-05 02:30:04 -0400

    completed deleting urls feature.

--- express_server.js
+++ express_server.js
@@ -56,6 +56,12 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${key}`);
 });
 
+// Route to delete URL Resource
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
