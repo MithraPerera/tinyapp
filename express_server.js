c7bb2b9e57a99219a9ff19b9248b0e8558fc94e5
Mithra Perera <mithraperera@hotmail.com>
2023-06-06 02:12:44 -0400

    added user logout feature

--- express_server.js
+++ express_server.js
@@ -90,6 +90,12 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+// User Logout Route
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
