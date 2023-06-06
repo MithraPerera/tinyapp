9ef7c39026985e9ca1ca82ec2eca5f69e710b9bd
Mithra Perera <mithraperera@hotmail.com>
2023-06-05 23:20:27 -0400

    implemented username login with associated cookie

--- express_server.js
+++ express_server.js
@@ -70,6 +70,14 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+// Username Login Route
+app.post("/login", (req, res) => {
+  const username = req.body["username"];
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
